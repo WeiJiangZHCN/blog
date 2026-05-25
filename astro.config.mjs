3bd2df7bd94a9ac5314bd8ac4ca673d474e467d8
chingsheng <2627218531@qq.com>
2026-05-25 11:43:59 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,8 @@ import rehypeLazyLoadImage from './src/plugins/lazyLoadImage.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://weijiangzhcn.github.io',
+  base: '/blog/',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex, rehypeLazyLoadImage],
